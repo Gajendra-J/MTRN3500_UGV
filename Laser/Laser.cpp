b5b7a839845fff83088edb54ef3ef5de3942b812
Gajendra Jayasekera <z5260252@ad.unsw.edu.au>
2021-10-12 12:38:14 +1100

    Setup laser to read raw data based off given code

--- Laser/Laser.cpp
+++ Laser/Laser.cpp
@@ -9,8 +9,13 @@
 using namespace System;
 using namespace System::Threading;
 
+using namespace System::Net::Sockets;
+using namespace System::Net;
+using namespace System::Text;
+
 int main()
 {
+    // SHARED MEMORY
     // Setting up shared Memory Objects and providing Create/Access
     SMObject PMObj(_TEXT("PMObj"), sizeof(ProcessManagement));
     SMObject LaserObj(_TEXT("Laser"), sizeof(SM_Laser));
@@ -28,6 +33,54 @@ int main()
     PMData->Shutdown.Flags.Laser = 0;
     int WaitAndSeeTime = 0;
 
+    // LASER
+    // LMS151 port number must be 23000
+    int PortNumber = 23000;    
+    // Pointer to TcpClent type object on managed heap
+    TcpClient^ Client;
+    // arrays of unsigned chars to send and receive data
+    array<unsigned char>^ SendData;
+    array<unsigned char>^ ReadData;
+    // String command to ask for Channel 1 analogue voltage from the PLC
+    // These command are available on Galil RIO47122 command reference manual
+    // available online
+    String^ AskScan = gcnew String("sRN LMDscandata");
+    String^ Auth = gcnew String("z5260252\n");
+    // String to store received data for display
+    String^ ResponseData;
+
+    // Creat TcpClient object and connect to it
+    Client = gcnew TcpClient("192.168.1.200", PortNumber);
+    // Configure connection
+    Client->NoDelay = true;
+    Client->ReceiveTimeout = 500;//ms
+    Client->SendTimeout = 500;//ms
+    Client->ReceiveBufferSize = 1024;
+    Client->SendBufferSize = 1024;
+
+    // unsigned char arrays of 16 bytes each are created on managed heap
+    SendData = gcnew array<unsigned char>(16);
+    ReadData = gcnew array<unsigned char>(2500);
+
+    // Get the network streab object associated with clien so we 
+    // can use it to read and write
+    NetworkStream^ Stream = Client->GetStream();
+
+    // Authenticate user
+    // Convert string command to an array of unsigned char
+    SendData = System::Text::Encoding::ASCII->GetBytes(Auth);
+    Stream->Write(SendData, 0, SendData->Length);
+    // Wait for the server to prepare the data, 1 ms would be sufficient, but used 10 ms
+    System::Threading::Thread::Sleep(10);
+    // Read the incoming data
+    Stream->Read(ReadData, 0, ReadData->Length);
+    // Convert incoming data from an array of unsigned char bytes to an ASCII string
+    ResponseData = System::Text::Encoding::ASCII->GetString(ReadData);
+    // Print the received string on the screen - AUTH CHECK - OK
+    Console::WriteLine(ResponseData);
+
+    SendData = System::Text::Encoding::ASCII->GetBytes(AskScan);
+
     while (!PMData->Shutdown.Flags.Laser)
     {
         // Set heartbeat flag
@@ -37,6 +90,27 @@ int main()
         {
             PMData->PMHeartbeat.Flags.Laser = 0;
             WaitAndSeeTime = 0;
+
+
+            // Write command asking for data
+            Stream->WriteByte(0x02);
+            Stream->Write(SendData, 0, SendData->Length);
+            Stream->WriteByte(0x03);
+            // Wait for the server to prepare the data, 1 ms would be sufficient, but used 10 ms
+            System::Threading::Thread::Sleep(10);
+            // Read the incoming data
+            Stream->Read(ReadData, 0, ReadData->Length);
+            // Convert incoming data from an array of unsigned char bytes to an ASCII string
+            ResponseData = System::Text::Encoding::ASCII->GetString(ReadData);
+
+
+            // Print the received string on the screen - RAW DATA
+            Console::WriteLine(ResponseData);
+
+
+            // CONVERT RAW DATA TO X Y USING LECTURER CODE MODEL HERE - NOTE JAMES SAID SOMETHING ABOUT CONVERTING TO DEGREES
+
+
         }
         else
         {
@@ -55,6 +129,9 @@ int main()
 
         Thread::Sleep(20);
     }
+
+    Stream->Close();
+    Client->Close();
 
 	return 0;
 }
