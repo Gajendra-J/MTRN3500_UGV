8745e4b0d9d002f1c9e3adb2f72c54456054eaa2
Gajendra Jayasekera <z5260252@ad.unsw.edu.au>
2021-10-13 10:22:22 +1100

    Setup class stubs for vehicle

---
+++ VehicleControlModule/VehicleControlClasses.cpp
@@ -0,0 +1,42 @@
+
+#include "VehicleControlClasses.h"
+
+int VehicleControl::connect(String^ hostName, int portNumber)
+{
+	// YOUR CODE HERE
+	return 1;
+}
+int VehicleControl::setupSharedMemory()
+{
+	// YOUR CODE HERE
+	return 1;
+}
+int VehicleControl::getData()
+{
+	// YOUR CODE HERE
+	return 1;
+}
+int VehicleControl::checkData()
+{
+	// YOUR CODE HERE
+	return 1;
+}
+int VehicleControl::sendDataToSharedMemory()
+{
+	// YOUR CODE HERE
+	return 1;
+}
+bool VehicleControl::getShutdownFlag()
+{
+	// YOUR CODE HERE
+	return 1;
+}
+int VehicleControl::setHeartbeat(bool heartbeat)
+{
+	// YOUR CODE HERE
+	return 1;
+}
+VehicleControl::~VehicleControl()
+{
+	// YOUR CODE HERE
+}
---
+++ VehicleControlModule/VehicleControlClasses.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <UGV_module.h>
+#include <smstructs.h>
+
+ref class VehicleControl : public UGV_module
+{
+
+public:
+	int connect(String^ hostName, int portNumber) override;
+	int setupSharedMemory() override;
+	int getData() override;
+	int checkData() override;
+	int sendDataToSharedMemory() override;
+	bool getShutdownFlag() override;
+	int setHeartbeat(bool heartbeat) override;
+	~VehicleControl();
+
+protected:
+	// YOUR CODE HERE (ADDITIONAL MEMBER VARIABLES THAT YOU MAY WANT TO ADD)
+
+};
