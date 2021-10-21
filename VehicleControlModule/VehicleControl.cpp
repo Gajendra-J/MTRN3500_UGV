5d55dc2a4584ec66b4f28aef1e2f6dc2bab8c1ed
Gajendra Jayasekera <z5260252@ad.unsw.edu.au>
2021-10-21 16:44:36 +1100

    Setup vehicle with some stub values for steer and speed to test if data was being sent

--- VehicleControlModule/VehicleControl.cpp
+++ VehicleControlModule/VehicleControl.cpp
@@ -27,6 +27,42 @@ int main()
     // Local Setup
     PMData->Shutdown.Flags.VehicleControl = 0;
     int WaitAndSeeTime = 0;
+    unsigned int flag = 0;
+    double Speed = 0.5;
+    double Steer = 30;
+
+    // VEHICLE
+    // Vehicle port number must be 25000
+    int PortNumber = 25000;
+    // Pointer to TcpClent type object on managed heap
+    TcpClient^ Client;
+    // arrays of unsigned chars to send data
+    array<unsigned char>^ SendData;
+
+    String^ Auth = gcnew String("5260252\n");
+
+    // Creat TcpClient object and connect to it
+    Client = gcnew TcpClient("192.168.1.200", PortNumber);
+    // Configure connection
+    Client->NoDelay = true;
+    Client->ReceiveTimeout = 2000;//ms LONGER FOR AUTH
+    Client->SendTimeout = 500;//ms
+    Client->ReceiveBufferSize = 1024;
+    Client->SendBufferSize = 1024;
+
+    // unsigned char arrays of 16 bytes each are created on managed heap
+    SendData = gcnew array<unsigned char>(50);
+
+    // Get the network streab object associated with clien so we 
+    // can use it to read and write
+    NetworkStream^ Stream = Client->GetStream();
+
+    // Authenticate user
+    // Convert string command to an array of unsigned char
+    SendData = System::Text::Encoding::ASCII->GetBytes(Auth);
+    Stream->Write(SendData, 0, SendData->Length);
+    // Resets buffer
+    SendData = gcnew array<unsigned char>(50);
 
     while (!PMData->Shutdown.Flags.VehicleControl)
     {
@@ -47,7 +83,23 @@ int main()
             }
         }
 
-        // Put Vehicle Control Module Code Below
+        // Vehicle things
+        // random testing values
+        Speed = -Speed;
+        Steer = -Steer;
+
+        // # <steer> <speed> <flag> # format
+        String^ Controls = gcnew String("# " + Steer.ToString("f2") + " " + Speed.ToString("f2") + " " + flag + " #");
+        Console::WriteLine(Controls);
+        SendData = System::Text::Encoding::ASCII->GetBytes(Controls);
+
+        Stream->WriteByte(0x02);
+        Stream->Write(SendData, 0, SendData->Length);
+        Stream->WriteByte(0x03);
+
+        System::Threading::Thread::Sleep(100);
+
+        flag = !flag;
 
         if (_kbhit())
         {
@@ -56,6 +108,9 @@ int main()
 
         Thread::Sleep(20);
     }
+
+    Stream->Close();
+    Client->Close();
 
     return 0;
 }
